2e1a1b8f3d05eec4f32e5bd1f1866339cb732b4c
gpusystem <lionmini@naver.com>
2022-06-28 10:15:52 +0000

    integrated no gpu script into gup script

--- ubuntu_no_gpu.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sed -i 's/1/0/g' /etc/apt/apt.conf.d/20auto-upgrades
-apt install -y net-tools nfs-common
-parted -s -a optimal -- /dev/sda mklabel gpt mkpart primary xfs 1 -1
-mkdir /data
-mkfs.xfs /dev/sda1
-echo "/dev/sda1	/data	xfs	defaults	0	0" >> /etc/fstab
-mount -a
