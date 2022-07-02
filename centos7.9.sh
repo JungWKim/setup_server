8871c153dc1979c9f2167dd0b5a5529061c1e3f9
Jeong Woo Kim <50034678+JungWKim@users.noreply.github.com>
2022-07-03 00:34:23 +0900

    Update centos7.9.sh

--- centos7.9.sh
+++ centos7.9.sh
@@ -79,20 +79,12 @@ fi
 
 #------------- install docker -------------
 if [ ${docker_install} = yes ] || [ ${docker_install} == y ]; then
-	yum remove docker \
-                   docker-client \
-                   docker-client-latest \
-                   docker-common \
-                   docker-latest \
-                   docker-latest-logrotate \
-                   docker-logrotate \
-                   docker-engine
 
 	yum install -y yum-utils
 	yum-config-manager \
 	    --add-repo \
 	    https://download.docker.com/linux/centos/docker-ce.repo
-	yum install docker-ce docker-ce-cli containerd.io -y
+	yum install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
 	systemctl start docker
 	systemctl enable docker
 
